7e1860dafb21143a361f73d1b539883b39d6ed9f
Lane <pixeledpepper@gmail.com>
2020-07-22 06:53:03 -0400

    generate basic dungeons

--- ultimafrog.py
+++ ultimafrog.py
@@ -3,8 +3,9 @@ import tcod
 
 from engine import Engine
 from entity import Entity
-from game_map import GameMap
 from player_input import EventHandler
+from procgen import generate_dungeon
+
 
 def main() -> None:
     # Declare the size of the window
@@ -13,6 +14,10 @@ def main() -> None:
 
     map_width = 80
     map_height = 45
+
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
 
     # We need to set what font/tileset we're using
     # The most common is CHARMAP_CP437 (16x16 grid, previously "ASCII_INROW")
@@ -35,7 +40,14 @@ def main() -> None:
 
     entities = {player}
 
-    game_map = GameMap(map_width, map_height)
+    game_map = generate_dungeon(
+        max_rooms = max_rooms,
+        room_min_size = room_min_size,
+        room_max_size = room_max_size,
+        map_width = map_width,
+        map_height = map_height,
+        player = player
+    )
 
     engine = Engine(
         entities=entities,
